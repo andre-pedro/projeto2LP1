fd0b8bb50e18192d124f78611ecc9e2e80d0ba2d
André Santos <afbsantos1@gmail.com>
2018-06-10 13:05:34 +0100

    add grid,renderer,and state.cs

--- Projeto2_LP1/Projeto2_LP1/Program.cs
+++ Projeto2_LP1/Projeto2_LP1/Program.cs
@@ -8,6 +8,10 @@ namespace Projeto2_LP1
 {
     class Program
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
         }
---
+++ Projeto2_LP1/Projeto2_LP1/State.cs
@@ -0,0 +1,7 @@
+﻿
+namespace Projeto2_LP1
+{
+    public enum State { Undecided, seen, unexplored}
+    
+    }
+
