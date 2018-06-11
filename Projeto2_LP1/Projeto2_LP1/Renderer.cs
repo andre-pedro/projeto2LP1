776b459a3d851c2c4561bbb0c973fe8c65e7e139
André Santos <afbsantos1@gmail.com>
2018-06-11 20:16:00 +0100

    working with array [8,8]

--- Projeto2_LP1/Projeto2_LP1/Renderer.cs
+++ Projeto2_LP1/Projeto2_LP1/Renderer.cs
@@ -29,16 +29,28 @@ namespace Projeto2_LP1
             int colinafinal = array.GetLength(1);
 
             for (int linha = 0; linha < 8; linha++)
-            {
-          
-                Console.WriteLine($"{array[linha,0]}");
                 for (int coluna = 0; coluna < 8; coluna++)
-                {
-       
-                }
-                
-            }
-            
+                  //  array[linha,coluna] = 
+                    Console.WriteLine($"{array[linha,0]}");
+
+
+            /*Malta esta parte esta correcta. mas tou com duvidas agora na parte seguinte:
+
+
+                  tipo o nosso array nao tem nada la dentro foi so construido 8 por 8, logo ao correr o programa vais vos aparecer "undecided" porque nao tem nada
+
+                  o que se pretende e que dentro de cada "casa" do array introduzamos "-" que neste caso na class state e a nossa explored, mas como nesta fase nao e preciso
+
+                  implementar o explorado e nao explorado tentem arranjar maneira de introduzir o - de outra maneira
+
+                  note: pff nao introduzam desta forma  int[,] values=new int[2,3]{{2,4,5},{4,5,2}};  
+                  isto nao nos vais ajudar em nada porque mais tarde vms voltar ao mesmo
+                  
+             thanks in advance*/
+
+
+
+
         }
     }
 }
