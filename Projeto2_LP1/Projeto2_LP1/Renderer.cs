e7df70f340c75bf0be434cd83d58729f2daba521
André Santos <afbsantos1@gmail.com>
2018-06-13 20:01:22 +0100

    working with array multi

--- Projeto2_LP1/Projeto2_LP1/Renderer.cs
+++ Projeto2_LP1/Projeto2_LP1/Renderer.cs
@@ -5,42 +5,37 @@ namespace Projeto2_LP1
 {
     class Renderer
     {
-     // private readonly int Width;
-      //private readonly int Height;
+        // private readonly int Width;
+        //private readonly int Height;
 
-      /*public Renderer(int width, int height)
-        {
-          this.Width = width;
-          this.Height = height;
-        }*/
+        /*public Renderer(int width, int height)
+          {
+            this.Width = width;
+            this.Height = height;
+          }*/
 
-          
 
         public static void Render(Grid grid)
         {
-           // State<>[,] array = new State[8, 8];
+            // State<>[,] array = new State[8, 8];
 
-            String [,] simbolos = new string[8,8];
+            String[,] simbolos = new string[8, 8];
+           //     String[,][] simbolos = new string[8, 8][];
 
-
-            //int linhafinal = array.GetLength(0);
-            //   int colinafinal = array.GetLength(1);
-
-            for (int linha = 0; linha < 8/2 ; linha++)
-                for (int coluna = 0; coluna < 8; coluna++)
+            for (int linha = 0; linha < 8; linha++)
+                for (int coluna = 0; coluna < 8 ; coluna++)
                     simbolos[linha, coluna] = "----";
-            //     Console.Write("-");
 
 
-            for (int linha = 0; linha < 8 / 2; linha++)
+
+
+            for (int linha = 0; linha < 8/2; linha++)
                 for (int coluna = 0; coluna < 8; coluna++)
-                    Console.WriteLine(simbolos[linha,coluna]);
+                {
+                    Console.WriteLine(simbolos[linha, coluna]);
 
-            
-            //    array[linha, coluna] = State.o;
-                //   Console.WriteLine($"{array[linha,coluna]}");
-          //     Console.Write("-");
-          //   Console.Write($"{array[linha,coluna]}");
+                }
+
 
 
 
@@ -58,10 +53,6 @@ namespace Projeto2_LP1
                   isto nao nos vais ajudar em nada porque mais tarde vms voltar ao mesmo
                   
              thanks in advance*/
-
-
-
-
         }
     }
 }
