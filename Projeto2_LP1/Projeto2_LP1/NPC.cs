24e7594660d27978ad4a92161d4b6c93ab6a7df8
synpse <tfaon@sapo.pt>
2018-06-14 15:21:28 +0100

    Create NPC class that extends Character

---
+++ Projeto2_LP1/Projeto2_LP1/NPC.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto2_LP1
+{
+    public class NPC : Character
+    {
+        // Substituicao do metodo Move()
+        public override char Move()
+        {
+            // Instancia random
+            Random rand = new Random();
+            // Variavel que guarda direcao
+            char direction = ' ';
+
+            // Escolher diracao a partir de random (entre 1 e 5)
+            switch (rand.Next(1, 5))
+            {
+                // Caso random seja 1
+                case 1:
+                    // Norte
+                    direction = 'N';
+                    break;
+                // Caso random seja 2
+                case 2:
+                    // Sul
+                    direction = 'S';
+                    break;
+                // Caso random seja 3
+                case 3:
+                    // Este
+                    direction = 'E';
+                    break;
+                // Caso random seja 4
+                case 4:
+                    // Oeste
+                    direction = 'W';
+                    break;
+            }
+
+            // Returns direction
+            return direction;
+        }
+    }
+}
