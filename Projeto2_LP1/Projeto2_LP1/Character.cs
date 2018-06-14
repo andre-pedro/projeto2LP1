2cac237f665f6e918bdfc82437bb93abbff52804
synpse <tfaon@sapo.pt>
2018-06-14 15:11:00 +0100

    Add Character Abstract Class

---
+++ Projeto2_LP1/Projeto2_LP1/Character.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto2_LP1
+{
+    public abstract class Character
+    {
+        // Propriedade
+        public string Name { get; }
+
+        // Methodo que escolhe direcao
+        public abstract char Move();
+    }
+}
