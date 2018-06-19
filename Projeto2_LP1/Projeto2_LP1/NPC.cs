9fd25290becc96513a81fa466050dfaa3264d4b0
hardcorelif3 <33433474+hardcorelif3@users.noreply.github.com>
2018-06-19 03:20:22 +0100

    changes to the NPC class

--- Projeto2_LP1/Projeto2_LP1/NPC.cs
+++ Projeto2_LP1/Projeto2_LP1/NPC.cs
@@ -7,8 +7,13 @@ namespace Projeto2_LP1
 
         public string Name { get; set; }
         public string Symbol { get; set; }
+        public string SymbolNeutral { get; set; }
+        public string SymbolHostile { get; set; }
         public float Hp { get; set; }
         public float Ap { get; set; }
+        public bool Neutral { get; set; }
+
+
     }
 }
 }
