1f99bb45029b3c60698ae6f4da85b2bc0bc93af9
synpse <tfaon@sapo.pt>
2018-06-19 21:50:43 +0100

    changes to GameOver

--- Projeto2_LP1/Projeto2_LP1/GameOver.cs
+++ Projeto2_LP1/Projeto2_LP1/GameOver.cs
@@ -10,7 +10,17 @@ namespace Projeto2_LP1
     {
         public void Die(Initializer init)
         {
-
+            Console.Clear();
+            Console.SetCursorPosition(55, 2);
+            Console.WriteLine(" [Game Over]");
+            Console.ResetColor();
+            Console.SetCursorPosition(42, 4);
+            Console.WriteLine($" You've died! You made it till level {init.level}!");
+            Console.SetCursorPosition(48, 6);
+            Console.WriteLine($" Press any Key to continue...");
+            Console.ReadKey();
+            Console.SetCursorPosition(48, 6);
+            Environment.Exit(0);
         }
     }
 }
