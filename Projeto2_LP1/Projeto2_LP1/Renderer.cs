2bb633f38b2465e59fc66a680920ad74156336f4
synpse <tfaon@sapo.pt>
2018-06-19 04:28:43 +0100

    lots of changes to render

--- Projeto2_LP1/Projeto2_LP1/Renderer.cs
+++ Projeto2_LP1/Projeto2_LP1/Renderer.cs
@@ -1,86 +1,62 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Projeto2_LP1
 {
     class Renderer
     {
-        public static IEnumerable<Grid> simbolos { get; private set; }
 
-        public static void Render()
+        public void Render(Initializer init, Grid grid)
         {
-            Grid grid = new Grid();
-            Tiles simbol = new Tiles();
+            Console.Clear();
 
-            //  String[,] simbolos = new string[8, 8];
-            //  String[,][] simbolos = new string[8, 8][];
-            // List<T>[,];
-
-            /*
-            //este funciona sem simbolos
-            for (int i = 0; i < 8; i++)
+            for (int row = 0; row < 8; row++)
             {
-                for (int j = 0; j < 8; j++)
-                {
-                    for (int k = 0; k < 5; k++)
-                    {
-                       // Console.Write(".");
-                        Console.Write(simbol);
-                    }
-                    Console.Write("\t");
+                Console.WriteLine("\n");
 
-                }
-                Console.WriteLine();
-                for (int j = 0; j < 8; j++)
+                for (int column = 0; column < 8; column++)
                 {
-                    for (int k = 0; k < 5; k++)
+                    if (column == 0)
                     {
-                       // Console.WriteLine("\t");
-                        Console.Write(".");
-
+                        Console.Write(" ");
                     }
-                    Console.Write("\t");
-                    
-                    //  Console.Write("\t");
+                    foreach (IGameObject thing in grid.array[row, column])
+                    {
+                        if (thing == init.player)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Cyan;
+                            Console.Write(thing.ToString());
+                            Console.ResetColor();
+                        }
+                    }
+                    for (int i = 5 - grid.array[row, column].Count(); i > 0; i--)
+                    {
+                        if (grid.array[row, column].Contains(init.exit))
+                        {
+                            // Não imprime (para não sobrepor exit)
+                        }
+                        else
+                        {
+                            if (grid.array[row, column].Contains(init.player))
+                            {
+                                Console.Write(". ");
+                            }
+                        }
+                    }
+
+                    if (column < 7)
+                    {
+                        Console.Write("\t");
+                    }
                 }
-                Console.WriteLine();
-                Console.WriteLine();
             }
-            //done here, thest bottom.
-            */
-
-            for (int i = 0; i < 8; i++)
-            {
-                for (int j = 0; j < 8; j++)
-                {
-                    //neste foreach e preciso passar os objectos para aqui
-                    //como, ainda nao sei =/
-                    foreach(Grid sim in simbolos)
-                    {
-                        Console.Write(sim);
-                    }
-                    Console.Write("\t");
-
-                }
-                Console.WriteLine();
-                for (int j = 0; j < 8; j++)
-                {
-                    for (int k = 0; k < 5; k++)
-                    {
-                        // Console.WriteLine("\t");
-                        Console.Write(".");
-
-                    }
-                    Console.Write("\t");
-
-                    //  Console.Write("\t");
-                }
-                Console.WriteLine();
-                Console.WriteLine();
-            }
-
-
-
+            Console.SetCursorPosition(55, 35);
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(" [Messages]");
+            Console.ResetColor();
         }
     }
 }
