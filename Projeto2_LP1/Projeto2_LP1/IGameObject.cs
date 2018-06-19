74d9b6acf616c60b94157321ecd8ba7eac1a09c0
synpse <tfaon@sapo.pt>
2018-06-19 03:02:32 +0100

    Add GameObject Interface

---
+++ Projeto2_LP1/Projeto2_LP1/IGameObject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto2_LP1
+{
+    interface IGameObject
+    {
+        string Name { get; set; }
+        string Symbol { get; set; }
+    }
+}
