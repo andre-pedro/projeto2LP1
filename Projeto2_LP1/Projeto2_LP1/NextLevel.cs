6164cc6ef1c21d84c866fe8d9cf9329d4884ec48
synpse <tfaon@sapo.pt>
2018-06-19 21:20:21 +0100

    Create class NextLevel

---
+++ Projeto2_LP1/Projeto2_LP1/NextLevel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto2_LP1
+{
+    class NextLevel
+    {
+        public bool HasLeveled(Initializer init, Grid grid)
+        {
+
+        }
+    }
+}
