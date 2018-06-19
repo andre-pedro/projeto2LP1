8cae02787a9a42a16754722a776f24098d0593fd
hardcorelif3 <33433474+hardcorelif3@users.noreply.github.com>
2018-06-19 03:46:59 +0100

    Add Food.cs

---
+++ Projeto2_LP1/Projeto2_LP1/Food.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto2_LP1
+{
+    class Food : IGameObject
+    {
+        public string Name { get; set; }
+        public string Symbol { get; set; }
+    }
+}
