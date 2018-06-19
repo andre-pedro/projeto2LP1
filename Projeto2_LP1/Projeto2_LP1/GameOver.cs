e5b4b8e20b49b1d837193b359f7942deb46805a0
synpse <tfaon@sapo.pt>
2018-06-19 21:49:41 +0100

    add class GameOver

---
+++ Projeto2_LP1/Projeto2_LP1/GameOver.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto2_LP1
+{
+    class GameOver
+    {
+        public void Die(Initializer init)
+        {
+
+        }
+    }
+}
