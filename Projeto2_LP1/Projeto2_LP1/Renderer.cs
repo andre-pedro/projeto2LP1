bd8648a00c27934aed35da5df6e5c770bce4f143
synpse <tfaon@sapo.pt>
2018-06-19 16:40:07 +0100

    try

--- Projeto2_LP1/Projeto2_LP1/Renderer.cs
+++ Projeto2_LP1/Projeto2_LP1/Renderer.cs
@@ -1,11 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
-
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Projeto2_LP1
 {
     class Renderer
     {
+
         public void Render(Initializer init, Grid grid)
         {
             Console.Clear();
@@ -22,122 +25,116 @@ namespace Projeto2_LP1
                     }
                     foreach (IGameObject thing in grid.array[row, column])
                     {
-                        Console.ResetColor();
                         if (thing == init.player)
                         {
-                            Console.ForegroundColor = ConsoleColor.Cyan;
                             Console.Write(thing.ToString());
-                            Console.ResetColor();
                         }
                         if (thing == init.exit)
-                            if (grid.array[grid.playerX, grid.playerY].Contains(init.exit))
-                            {
-
-                                Console.ForegroundColor = ConsoleColor.White;
-                                Console.Write(thing.ToString());
-                                Console.ResetColor();
-                            }
-
-                            else
-                            {
-                                Console.Write("~ ~ ~ ~ ~ ");
-                            }
-
-
+                        {
+                            Console.Write(thing.ToString());
+                        }
                         if (thing == init.trap1)
                         {
-                            if (grid.array[grid.playerX, grid.playerY].Contains(init.trap1))
-                            {
-                                Console.ForegroundColor = ConsoleColor.Red;
-                                Console.Write(thing.ToString());
-                                Console.ResetColor();
-                            }
-                            else
-                            {
-                                Console.Write("~ ");
-                            }
+                            Console.Write(thing.ToString());
                         }
                         if (thing == init.trap2)
                         {
-                            if (grid.array[grid.playerX, grid.playerY].Contains(init.trap2))
-                            {
-                                Console.ForegroundColor = ConsoleColor.Red;
-                                Console.Write(thing.ToString());
-                                Console.ResetColor();
-                            }
-                            else
-                            {
-                                Console.Write("~ ");
-                            }
+                            Console.Write(thing.ToString());
                         }
                         if (thing == init.trap3)
                         {
-                            if (grid.array[grid.playerX, grid.playerY].Contains(init.trap3))
-                            {
-                                Console.ForegroundColor = ConsoleColor.Red;
-                                Console.Write(thing.ToString());
-                                Console.ResetColor();
-                            }
-                            else
-                            {
-                                Console.Write("~ ");
-                            }
+                            Console.Write(thing.ToString());
                         }
                         if (thing == init.npc)
                         {
-                            if (grid.array[grid.playerX, grid.playerY].Contains(init.npc))
-                            {
-                                Console.ForegroundColor = ConsoleColor.Yellow;
-                                Console.Write(thing.ToString());
-                                Console.ResetColor();
-                            }
-                            else
-                            {
-                                Console.Write("~ ");
-                            }
+                            Console.Write(thing.ToString());
                         }
-                        if (thing == init.food1)
+                    }
+                    for (int i = 5 - grid.array[row, column].Count(); i > 0; i--)
+                    {
+                        if (grid.array[row, column].Contains(init.exit))
                         {
-                            if (grid.array[grid.playerX, grid.playerY].Contains(init.food1))
-                            {
-                                Console.ForegroundColor = ConsoleColor.Green;
-                                Console.Write(thing.ToString());
-                                Console.ResetColor();
-                            }
-                            else
-                            {
-                                Console.Write("~ ");
-                            }
+                            // Não imprime (para não sobrepor exit)
                         }
-                        if (thing == init.food2)
+                        else
                         {
-                            if (grid.array[grid.playerX, grid.playerY].Contains(init.food2))
+                            if (grid.array[row, column].Contains(init.player))
                             {
-                                Console.ForegroundColor = ConsoleColor.Green;
-                                Console.Write(thing.ToString());
-                                Console.ResetColor();
-                            }
-                            else
-                            {
-                                Console.Write("~ ");
-                            }
-                        }
-                        if (thing == init.food3)
-                        {
-                            if (grid.array[grid.playerX, grid.playerY].Contains(init.food3))
-                            {
-                                Console.ForegroundColor = ConsoleColor.Green;
-                                Console.Write(thing.ToString());
-                                Console.ResetColor();
-                            }
-                            else
-                            {
-                                Console.Write("~ ");
+                                Console.Write(". ");
                             }
                         }
                     }
+
+                    if (column < 7)
+                    {
+                        Console.Write("\t");
+                    }
+
+                    if (column == 7)
+                    {
+                        if (row == 0)
+                        {
+                            Console.Write($"          [Status]        " +
+                                $"                [Info]              ");
+                        }
+                        if (row == 1)
+                        {
+                            Console.Write($"    Current Level:     ");
+                            Console.Write($"{init.level:000}              ");
+                            Console.ResetColor();
+                            Console.Write($"Player - ");
+                            Console.Write($"\u2302              ");
+                        }
+                        if (row == 2)
+                        {
+                            Console.Write($"    Player HP:         ");
+                            Console.Write($"{init.player.Hp:000}             ");
+                            Console.Write($"  Empty - ." +
+                                $"               ");
+                        }
+                        if (row == 3)
+                        {
+                            Console.Write($"                    ");
+                            Console.Write($"                      Exit - ");
+                            Console.Write($"E X I T !              ");
+                        }
+                        if (row == 4)
+                        {
+                            Console.Write($"                             ");
+                            Console.Write($"             Trap - ");
+                            Console.Write($"\u25B2   ");
+                        }
+                        if (row == 5)
+                        {
+                            Console.Write($"                             ");
+                            Console.Write($"             Food - ");
+                            Console.Write($"\u2665   ");
+                        }
+                        if (row == 6)
+                        {
+                            Console.Write($"                             ");
+                            Console.Write($"      Neutral NPC - ");
+                            Console.Write($"\u25CA              ");
+                        }
+                        if (row == 7)
+                        {
+                            Console.Write($"                             ");
+                            Console.Write($"      Hostile NPC - ");
+                            Console.Write($"\u2666              ");
+                        }
+                        Console.Write("\n");
+                    }
                 }
             }
+            Console.SetCursorPosition(16, 26);
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(" [Surroundings]");
+            Console.ResetColor();
+
+            Console.SetCursorPosition(55, 35);
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(" [Messages]");
+            Console.ResetColor();
         }
     }
 }
