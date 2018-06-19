ce9fab9f5ffdd2acf2a36017b43a6542454edb82
André Santos <afbsantos1@gmail.com>
2018-06-19 11:32:45 +0100

    add xml comments to food.cs

--- Projeto2_LP1/Projeto2_LP1/Food.cs
+++ Projeto2_LP1/Projeto2_LP1/Food.cs
@@ -6,12 +6,26 @@ using System.Threading.Tasks;
 
 namespace Projeto2_LP1
 {
+    /// <summary>
+    /// Esta classe herda da interface IgameObject. É composta por três 
+    /// propriedades, todas elas de com a possibilidade de escrita e leitura. 
+    /// </summary>
     class Food : IGameObject
     {
+
+        /// <summary>
+        /// Nestas três propriedades auto-implementadas conseguimos indicar e 
+        /// obter o nome, o simbolo e a quantidade de HP que o jogador consegue
+        /// recuperar
+        /// </summary>
         public string Name { get; set; }
         public string Symbol { get; set; }
         public float Heal { get; set; }
 
+        /// <summary>
+        /// O Constructor Food indica o nome da comida, o símbolo que foi escolhido
+        /// em utf-8 e a quantidade de vida que o jogador pode recuperar.
+        /// </summary>
         public Food()
         {
             Name = "Cat food";
@@ -19,6 +33,12 @@ namespace Projeto2_LP1
             Heal = 25;
         }
 
+
+        /// <summary>
+        /// Através do override é possivel modificar o método ToString para 
+        /// devolver o símbolo escolhido para a grid de visualização.
+        /// </summary>
+        /// <returns></returns>
         public override string ToString()
         {
             return Symbol;
