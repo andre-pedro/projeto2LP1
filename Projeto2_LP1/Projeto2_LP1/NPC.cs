ddfd072af968ec111f1eb378c39addbdbe159324
hardcorelif3 <33433474+hardcorelif3@users.noreply.github.com>
2018-06-19 03:14:27 +0100

    major changes to NPC

--- Projeto2_LP1/Projeto2_LP1/NPC.cs
+++ Projeto2_LP1/Projeto2_LP1/NPC.cs
@@ -2,43 +2,11 @@
 
 namespace Projeto2_LP1
 {
-    public class NPC : Character
+    public class NPC : IGameObject
     {
-        // Substituicao do metodo Move()
-        public override char Move()
-        {
-            // Instancia random
-            Random rand = new Random();
-            // Variavel que guarda direcao
-            char direction = ' ';
 
-            // Escolher diracao a partir de random (entre 1 e 5)
-            switch (rand.Next(1, 5))
-            {
-                // Caso random seja 1
-                case 1:
-                    // Norte
-                    direction = 'N';
-                    break;
-                // Caso random seja 2
-                case 2:
-                    // Sul
-                    direction = 'S';
-                    break;
-                // Caso random seja 3
-                case 3:
-                    // Este
-                    direction = 'E';
-                    break;
-                // Caso random seja 4
-                case 4:
-                    // Oeste
-                    direction = 'W';
-                    break;
-            }
-
-            // Retorna direcao
-            return direction;
-        }
+        public string Name { get; set; }
+        public string Symbol { get; set; }
     }
+}
 }
