03ade96437526a76fe71ee88a7311f6d54089c0e
André Santos <afbsantos1@gmail.com>
2018-06-20 22:57:22 +0100

    add map.cs xml

--- Projeto2_LP1/Projeto2_LP1/Map.cs
+++ Projeto2_LP1/Projeto2_LP1/Map.cs
@@ -1,11 +1,20 @@
 ﻿namespace Projeto2_LP1
 {
+
+    /// <summary>
+    /// Classe que implementa a interface IGameObject, com três propriedas auto-
+    /// -implementadas, um constructor e um método override de ToString();.
+    /// </summary>
     class Map : IGameObject
     {
         public string Name { get; set; }
         public string Symbol { get; set; }
         public bool Explored { get; set; }
 
+        /// <summary>
+        /// Constructor que permite indicar o símbolo o nome, e se é visível ao 
+        /// jogador.
+        /// </summary>
         public Map()
         {
             Name = "Map";
@@ -13,6 +22,11 @@
             Explored = false;
         }
 
+        /// <summary>
+        /// Método que através de override permit desenahr na grid o símbolo 
+        /// UTF-8 escolhido.
+        /// </summary>
+        /// <returns></returns>
         public override string ToString()
         {
             return Symbol;
