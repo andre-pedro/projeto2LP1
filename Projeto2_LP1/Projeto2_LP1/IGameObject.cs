62d7c36ca076d4af0143410d8f0c31f4c7258a1f
André Santos <afbsantos1@gmail.com>
2018-06-20 19:42:13 +0100

    done in IgameObject

--- Projeto2_LP1/Projeto2_LP1/IGameObject.cs
+++ Projeto2_LP1/Projeto2_LP1/IGameObject.cs
@@ -1,5 +1,10 @@
 ﻿namespace Projeto2_LP1
 {
+    /// <summary>
+    /// Interface constituida por duas propriedades auto-implementadas. Qualquer 
+    /// classe que implementar esta interface, têm a possibilidade de obter o 
+    /// respectivo nome e símbolo de cada objecto criado.
+    /// </summary>
     interface IGameObject
     {
         string Name { get; set; }
