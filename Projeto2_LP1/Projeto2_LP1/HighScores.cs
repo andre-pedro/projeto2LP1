c60bdb86ab93eb3c58ed984edf24e718820fabde
André Santos <afbsantos1@gmail.com>
2018-06-20 19:05:57 +0100

    add highscores xml comment

--- Projeto2_LP1/Projeto2_LP1/HighScores.cs
+++ Projeto2_LP1/Projeto2_LP1/HighScores.cs
@@ -2,10 +2,33 @@
 
 namespace Projeto2_LP1
 {
+    /// <summary>
+    /// Esta classe ira imprimir ao jogador na consola os HighScores do jogo.
+    /// </summary>
     class HighScores
     {
+        /// <summary>
+        /// É criada uma instância da classe HighScoresManager que permite 
+        /// revelar os jogadores que conseguiram obter melhor pontuação.
+        /// </summary>
         HighScoresManager hsm = new HighScoresManager();
 
+        /// <summary>
+        /// Através da utilização de métodos que a classe Console fornece ao método
+        /// Print(); permite posicionar o nome do jogador e as melhores pontuações
+        /// na consola. 
+        /// Os métodos utilizados da classe Console são:
+        /// - Clear(); - permite limpar a consola.
+        /// - SetCursorPosition(); - indica aonde irá ser colocado a informação 
+        /// desejada.
+        /// - WriteLine(); - escreve no ecrã o que pretendemos e dá um enter 
+        /// automático.
+        /// - ForegroundColor(); - Permite a utilização de cores com os nossos 
+        /// writelines();/writes();.
+        /// - Resetcolor(); - limpa a cor anteriormente escolhida.
+        /// - Readkey(); - Lê o input do utilizador para poder retroceder ao 
+        /// Mainmenu.
+        /// </summary>
         public void Print()
         {
             Console.Clear();
@@ -19,6 +42,7 @@ namespace Projeto2_LP1
 
             int row = 4;
 
+            ///Pecorre o ficheiro .txt e imprimi o nome e o respectivo score.
             foreach (Tuple<string, float> highscore in hsm.GetScores())
             {
                 row++;
