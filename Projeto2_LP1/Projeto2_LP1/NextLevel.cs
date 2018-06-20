9801fe7f0467bc087aef6b863aeef4cb2e2a3e0d
André Santos <afbsantos1@gmail.com>
2018-06-20 23:00:30 +0100

    add nextleve.cs

--- Projeto2_LP1/Projeto2_LP1/NextLevel.cs
+++ Projeto2_LP1/Projeto2_LP1/NextLevel.cs
@@ -2,13 +2,29 @@
 
 namespace Projeto2_LP1
 {
+
+    /// <summary>
+    /// Classe responsavél pela a alteração de nível.
+    /// </summary>
     class NextLevel
     {
+
+        /// <summary>
+        /// Método que permitve verificar se existe alteração de nível.
+        /// </summary>
+        /// <param name="init">Recebe tudo o que exite no nível, armadilhas, 
+        /// comida, etc..</param>
+        /// <param name="grid">Vai receber a grid do nível actual em que o 
+        /// jogador se encontra.</param>
+        /// <returns>Retorna a verificação se houve passagem de nível.</returns>
         public bool HasLeveled(Initializer init, Grid grid)
         {
             if (grid.array[grid.playerX, grid.playerY].Contains(init.player) &&
                 grid.array[grid.playerX, grid.playerY].Contains(init.exit) && init.player.Hp > 0)
             {
+
+                ///Caso a verificação seja válida, no novo nível irá apareer todos
+                ///os objectos como não visíveis.
                 init.exit.Explored = false;
                 init.trap1.Explored = false;
                 init.trap2.Explored = false;
@@ -22,7 +38,11 @@ namespace Projeto2_LP1
                 init.npc.Explored = false;
                 init.map.Explored = false;
                 init.level++;
+
+                ///Limpa a consola.
                 Console.Clear();
+
+                ///Informa ao jogador a alteração de nível.
                 Console.SetCursorPosition(0, 1);
                 Console.WriteLine(" ╔═════════════════════════════════════════════════════" +
                     "════════════════════════════════════════════════════════════════════╗");
@@ -42,6 +62,8 @@ namespace Projeto2_LP1
                 Console.Clear();
                 return true;
             }
+
+            ///Retorna falso caso não exista alteração de nível.
             return false;
         }
     }
